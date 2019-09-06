331b24f6f992f3fa6aa0fa5b54560e5784da9a51
chrisimholte <Flash#44>
2019-09-06 11:51:01 -0500

    Remember to email the appropriate file to nhalliga@macalester.edu

---
+++ src/basicjava/BasicStuff.java
@@ -0,0 +1,25 @@
+package basicjava;
+
+public class BasicStuff {
+
+    /**
+     * @author chris imholte
+     * @author nicholas halligan mesa
+     */
+
+    public static void main (String[] args){
+
+        double age0;
+        double age1;
+        age0 = 18;
+        age1 = 19;
+        System.out.println("We are " + age0 + " and " + age1 + " years old!");
+
+        int sumOfAges;
+        sumOfAges = (int) (age0 + age1);
+        System.out.println("The sum of our ages is " + sumOfAges + "!");
+        System.out.println("The sum of our ages is " + (age0 + age1) + "!");
+        System.out.println(6/3);
+        System.out.println(6/4);
+    }
+}
